7a91ee37ca3ef4a6fcb2476526411de9838adc81
Gang Wei <gang.wei@intel.com>
2014-01-30 13:08:17 +0800

    Update change log for 1.8.0
    Signed-off-by: Gang Wei <gang.wei@intel.com>

--- tboot/Config.mk
+++ tboot/Config.mk
@@ -32,7 +32,7 @@ CFLAGS		+= $(call cc-option,$(CC),-fno-stack-protector,)
 CFLAGS		+= $(call cc-option,$(CC),-fno-stack-protector-all,)
 
 # changeset variable for banner
-CFLAGS		+= -DTBOOT_CHANGESET=\""$(shell ((hg parents --template "{isodate|isodate} {rev}:{node|short}" >/dev/null && hg parents --template "{isodate|isodate} {rev}:{node|short}") || echo "2013-07-05 12:00 +0800 1.7.4") 2>/dev/null)"\"
+CFLAGS		+= -DTBOOT_CHANGESET=\""$(shell ((hg parents --template "{isodate|isodate} {rev}:{node|short}" >/dev/null && hg parents --template "{isodate|isodate} {rev}:{node|short}") || echo "2014-01-30 12:00 +0800 1.8.0") 2>/dev/null)"\"
 
 
 AFLAGS		+= -D__ASSEMBLY__
