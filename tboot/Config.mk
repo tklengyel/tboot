0ead0fcb565353bde9aa795bc431df5cc7cf66fa
Ning Sun <ning.sun@intel.com>
2015-05-07 14:57:39 -0700

    Disable -fstack-check in CFLAG for compatibility with Gentoo Linux.
    Signed-off-by: Jason Zaman <jason@perfinion.com>
    Signed-off-by: Ning Sun <ning.sun@intel.com>

--- tboot/Config.mk
+++ tboot/Config.mk
@@ -30,6 +30,7 @@ CFLAGS		+= -msoft-float
 CFLAGS		+= $(call cc-option,$(CC),-nopie,)
 CFLAGS		+= $(call cc-option,$(CC),-fno-stack-protector,)
 CFLAGS		+= $(call cc-option,$(CC),-fno-stack-protector-all,)
+CFLAGS		+= $(call cc-option,$(CC),-fno-stack-check,)
 
 # changeset variable for banner
 CFLAGS		+= -DTBOOT_CHANGESET=\""$(shell ((hg parents --template "{isodate|isodate} {rev}:{node|short}" >/dev/null && hg parents --template "{isodate|isodate} {rev}:{node|short}") || echo "2015-05-08 12:00 -0800 1.8.3") 2>/dev/null)"\"
